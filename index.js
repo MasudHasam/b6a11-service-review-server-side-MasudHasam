275664a3b26e01748b289ce1ac837d4e12303ae3
Masud Hasan <masudhasamf@gmail.com>
2022-11-09 05:45:29 +0600

    load single data using id done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,6 +30,16 @@ async function run() {
             const result = await cursor.limit(numLimit).toArray();
             // console.log(limit, numLimit);
             res.send(result)
+        });
+
+
+        //load data according to id;
+        app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const cursor = foodCollection.findOne(query);
+            const result = await cursor;
+            res.send(result);
         })
 
     }
